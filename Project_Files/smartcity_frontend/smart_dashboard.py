d5f04ea3f01812dec56b9e4282dcd2822d0d3e4b
Harini-devx <harinin459@gmail.com>
2025-06-28 15:36:28 +0530

    Fix sys.path to resolve app module in Streamlit

--- Project_Files/smartcity_frontend/smart_dashboard.py
+++ Project_Files/smartcity_frontend/smart_dashboard.py
@@ -1,3 +1,10 @@
+import sys
+import os
+
+# Dynamically add 'Project_Files' to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+
 import streamlit as st
 import requests
 from fpdf import FPDF
